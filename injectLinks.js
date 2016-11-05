334c2691681e2f611d0533bdd3a0c8c3da13de69
Ken Hung <eiekenhung@gmail.com>
2016-11-05 08:35:46 +0800

    remove draft spec

--- injectLinks.js
+++ injectLinks.js
@@ -9,6 +9,33 @@ for (var i = 0; i < bodyTextNodes.length; i++) {
   replaceWithNodes(bodyTextNodes[i], ezraNodes);
 }
 
+$(document).tooltip({
+  content: function () {
+    return $(this).prop('title');
+  },
+  open: function (event, ui) {
+    if (typeof (event.originalEvent) === 'undefined') {
+      return false;
+    }
+    // close any lingering tooltips
+    var $id = $(ui.tooltip).attr('id');
+    $('div.ui-tooltip').not('#' + $id).remove();
+  },
+  close: function (event, ui) {
+    // keep tooltip open on hover
+    ui.tooltip.hover(
+      function () {
+        $(this).stop(true).fadeTo(400, 1);
+      },
+      function () {
+        $(this).fadeOut('400', function () {
+          $(this).remove();
+        });
+      }
+    );
+  }
+});
+
 function getTextNodesIn(node, includeWhitespaceNodes) {
     var textNodes = [];
     function getTextNodes(node) {
