2f8f10a46cb57c8ab433b4be9eafb311c717a723
Ken Hung <eiekenhung@gmail.com>
2016-11-29 21:14:48 +0800

    unit test for FHL case

--- tests.js
+++ tests.js
@@ -58,6 +58,7 @@ QUnit.test("BibleRefReader.linkify", function (assert) {
     linkifyTest('約翰福音1:1', '<a title="載入中...(約翰福音1:1)" class="ezraBibleRefLink">約翰福音1:1</a>');
     linkifyTest('約四24，', '<a title="載入中...(約四24)" class="ezraBibleRefLink">約四24</a>，');
     linkifyTest('約四，', '約四，');
+    linkifyTest('李約 2013.11.17', '李約 2013.11.17');
     linkifyTest('希伯來書四章8節', '<a title="載入中...(希伯來書四章8節)" class="ezraBibleRefLink">希伯來書四章8節</a>');
     linkifyTest('詩篇一百一十八篇8至9節', '<a title="載入中...(詩篇一百一十八篇8至9節)" class="ezraBibleRefLink">詩篇一百一十八篇8至9節</a>');
 });
