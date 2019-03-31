8786fb82c6f8a0538982a586fd96f1a29bc17c08
Ken Hung <eiekenhung@gmail.com>
2019-03-31 15:21:38 +0800

    fix issue in latest Chrome by async XHR

--- ezra.sc.js
+++ ezra.sc.js
@@ -435,36 +435,38 @@ var Drop=function(t){"use strict";function e(t,e){if(!(t instanceof e))throw new
                   + '&chap=' + chap 
                   + '&sec=' + vers 
                   + '&gb=' + Resources.fhl_gb;
-        xhr.open('GET', query, false);
-        xhr.send();
-        if (xhr.status !== 200) {
-          fail(Resources.err_cannot_find_verse + 'XHR status = ' + xhr.status);
-          return;
-        }
-        try {
-          var resp = JSON.parse(xhr.responseText);
-          if (resp.status !== 'success') {
-            fail(Resources.err_cannot_find_verse + 'FHL response text = ' + xhr.responseText);
-            return;
-          } else if (resp.record.length === 0) {
-            fail(Resources.err_no_record + refText + '？');
+        xhr.open('GET', query, true);
+        xhr.onload = function() {
+          if (xhr.status !== 200) {
+            fail(Resources.err_cannot_find_verse + 'XHR status = ' + xhr.status);
             return;
           }
-          var versesText = '';
-          var lastSec = 0;
-          for (var i = 0; i < resp.record.length; i++) {
-            var record = resp.record[i];
-            // insert '⋯⋯' if verses are not continuous
-            if (i > 0 && record.sec > lastSec + 1) {
-              versesText += '⋯⋯';
+          try {
+            var resp = JSON.parse(xhr.responseText);
+            if (resp.status !== 'success') {
+              fail(Resources.err_cannot_find_verse + 'FHL response text = ' + xhr.responseText);
+              return;
+            } else if (resp.record.length === 0) {
+              fail(Resources.err_no_record + refText + '？');
+              return;
             }
-            lastSec = record.sec;
-            versesText += record.bible_text;
+            var versesText = '';
+            var lastSec = 0;
+            for (var i = 0; i < resp.record.length; i++) {
+              var record = resp.record[i];
+              // insert '⋯⋯' if verses are not continuous
+              if (i > 0 && record.sec > lastSec + 1) {
+                versesText += '⋯⋯';
+              }
+              lastSec = record.sec;
+              versesText += record.bible_text;
+            }
+            success(versesText);
+          } catch (err) {
+            fail(Resources.err_cannot_find_verse + err);
           }
-          success(versesText);
-        } catch (err) {
-          fail(Resources.err_cannot_find_verse + err);
-        }
+        };
+        xhr.send();
       }
       catch (err) {
         fail(Resources.err_cannot_find_verse + err);
